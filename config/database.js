28efc6dc99f90c377abef55bec696ddf9b28ab11
Tukue Gebregergis <tukue.geb@gmail.com>
2025-02-05 21:46:02 +0100

    replace mockdata with data from db

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
-const {Pool} = require('pg');   
-const dotenv = require('./dotenv');
+const { Pool} = require('pg');   
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const  pool =  new  Pool({ 
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,44 +1,63 @@
-// Mock data
-let books = [
-  { id: 1, title: 'Mock Book 1', author: 'Author 1', rating: 5, notes: 'Notes for book 1' },
-  { id: 2, title: 'Mock Book 2', author: 'Author 2', rating: 4, notes: 'Notes for book 2' },
-];
+const pool = require('../../config/database');
 
 // Get all books
 const getAllBooks = async () => {
-  return books;
+  const query = 'SELECT * FROM books';
+  try {
+    const result = await pool.query(query);
+    return result.rows;
+  } catch (err) {
+    console.error('Error getting all books', err);
+    throw err;
+  }
 };
 
 // Add a new book
 const addBook = async (title, author, rating, notes) => {
-  const newBook = {
-    id: books.length + 1,
-    title,
-    author,
-    rating,
-    notes,
-  };
-  books.push(newBook);
-  return newBook;
+  const query = `
+    INSERT INTO books (title, author, rating, notes)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+  `;
+  const values = [title, author, rating, notes];
+  try {
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  } catch (err) {
+    console.error('Error adding new book', err);
+    throw err;
+  }
 };
 
 // Update a book
 const updateBook = async (id, title, author, rating, notes) => {
-  const index = books.findIndex(book => book.id === parseInt(id));
-  if (index !== -1) {
-    books[index] = { id: parseInt(id), title, author, rating, notes };
-    return books[index];
+  const query = `
+    UPDATE books
+    SET title = $1, author = $2, rating = $3, notes = $4
+    WHERE id = $5
+    RETURNING *;
+  `;
+  const values = [title, author, rating, notes, id];
+  try {
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  } catch (err) {
+    console.error('Error updating book', err);
+    throw err;
   }
-  return null;
 };
 
+// Delete a book
 const deleteBook = async (id) => {
-  const index = books.findIndex(book => book.id === parseInt(id));
-  if (index !== -1) {
-    const deletedBook = books.splice(index, 1);
-    return deletedBook[0];
+  const query = 'DELETE FROM books WHERE id = $1 RETURNING *;';
+  const values = [id];
+  try {
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  } catch (err) {
+    console.error('Error deleting book', err);
+    throw err;
   }
-  return null;
 };
 
 module.exports = {
